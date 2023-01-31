a1067cb6d42a7abf4a616c7151b4ea9434d0b00c
oil@duople.com <oil>
2023-02-01 02:00:07 +0900

    feat: class 예제 코드 추가

--- why-ts/math.ts
+++ why-ts/math.ts
@@ -5,3 +5,10 @@ const test1 = sum(10, 20);
 // sum('10', '20'); // Error: '10'은 number에 할당될 수 없습니다.
 
 console.log(test1);
+
+class Student {
+  constructor(public name: string, public age: number) {
+    this.name = name;
+    this.age = age;
+  }
+}
